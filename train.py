1bbd3ff7128f215825f2f4710738d9dce18faa44
Adyoshkin Vasiliy <adeshkin.vi@phystech.edu>
2021-03-13 12:36:27 +0300

    add modules

--- train.py
+++ train.py
@@ -1,12 +1,9 @@
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pytorch_lightning.callbacks import ModelCheckpoint
 import wandb
 
 from dataset_light import CustomDataModule
 from model import LitModel
-from callbacks import ImagePredictionLogger
+from callbacks import early_stop_callback, checkpoint_callback, wandb_logger, ImagePredictionLogger
 
 
 def main():
@@ -18,40 +15,32 @@ def main():
     height, width = input_size
     input_shape = (num_channels, height, width)
 
+    image_dir = ""
+    annotation_path = ""
+    category = ""
+    batch_size = 16
+
+    dm = CustomDataModule(image_dir, annotation_path, category, batch_size, input_size)
+    dm.setup()
+
+    val_samples = next(iter(dm.val_dataloader()))
+
     model = LitModel(input_shape, num_classes, learning_rate)
-
-    wandb_logger = WandbLogger(project='wandb-lightning', job_type='train')
-
-    early_stop_callback = EarlyStopping(
-        monitor='val_loss',
-        patience=3,
-        verbose=False,
-        mode='min'
-    )
-
-    MODEL_CKPT_PATH = 'model/'
-    MODEL_CKPT = 'model/model-{epoch:02d}-{val_loss:.2f}'
-
-    checkpoint_callback = ModelCheckpoint(
-        monitor='val_loss',
-        filename=MODEL_CKPT,
-        save_top_k=3,
-        mode='min')
 
     trainer = pl.Trainer(max_epochs=150,
                          progress_bar_refresh_rate=20,
                          gpus=1,
                          logger=wandb_logger,
                          callbacks=[early_stop_callback,
-                                    ImagePredictionLogger(val_samples, 64)],
+                                    ImagePredictionLogger(val_samples, batch_size)],
                          checkpoint_callback=checkpoint_callback)
 
     # Train the model
     trainer.fit(model, dm)
 
-
     # Close wandb run
     wandb.finish()
+
 
 if __name__ == "__main__":
     main()
